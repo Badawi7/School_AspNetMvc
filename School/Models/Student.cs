8db0398c1ab5e87bdd4fea622062524eede6a94b
Badawi7 <badawi2005@hotmail.com>
2022-06-17 14:14:52 +0300

    Student model added
    A class was defined for a Student model.

---
+++ School/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School.Models
+{
+  public class Student
+  {
+    public Student(int id)
+    {
+      Id = id;
+    }
+
+    public int Id { get; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+  }
+}
