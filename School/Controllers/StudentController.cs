15bda91fffde8561fdf76a998a914e73b54cf255
Badawi7 <badawi2005@hotmail.com>
2022-06-18 12:03:05 +0300

    Create/Edit Student functionalities added
    Implemented student addition and editing functionalities, and modified the Student model class.

--- School/Controllers/StudentController.cs
+++ School/Controllers/StudentController.cs
@@ -9,15 +9,57 @@ namespace School.Controllers
 {
   public class StudentController : Controller
   {
+    static readonly List<Student> students = new List<Student> {
+        new Student() {Id = 1, Name = "Chris", Age = 15},
+        new Student() {Id = 2, Name = "Leon", Age = 14},
+        new Student() {Id = 3, Name = "Albert", Age = 15}
+      };
+
+    private int GetNewId()
+    {
+      return students.Max(s => s.Id) + 1;
+    }
+
     // GET: Student
     public ActionResult Index()
     {
-      Student[] students = {
-        new Student(1) {Name = "Chris", Age = 15},
-        new Student(2) {Name = "Leon", Age = 14},
-        new Student(3) {Name = "Albert", Age = 15}
-      };
       return View(students);
+    }
+
+    public ActionResult Edit(int? id)
+    {
+      if (id == null)
+      {
+        return View();
+      }
+      else
+      {
+        var student = students.Find(s => s.Id == id);
+        return View(student);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Student student)
+    {
+      System.Diagnostics.Debugger.Break();
+      if (student.Id == 0)
+      {
+        student.Id = GetNewId();
+        students.Add(student);
+      }
+      else
+      {
+        var targetStudent = students.Find(s => s.Id == student.Id);
+        targetStudent.Name = student.Name;
+        targetStudent.Age = student.Age;
+      }
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Create()
+    {
+      return RedirectToAction("Edit");
     }
   }
 }
--- School/Models/Student.cs
+++ School/Models/Student.cs
@@ -7,12 +7,7 @@ namespace School.Models
 {
   public class Student
   {
-    public Student(int id)
-    {
-      Id = id;
-    }
-
-    public int Id { get; }
+    public int Id { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
   }
